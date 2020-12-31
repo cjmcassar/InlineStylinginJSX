1a2bdd3a19ca674386c1d14f74baeb00e12f2be8
Christopher-John Cassar <46441540+cjmcassar@users.noreply.github.com>
2020-12-31 13:47:08 +0000

    Added inline property changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customeStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customeStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customeStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
